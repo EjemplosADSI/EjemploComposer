41bebc8ecca2c38f3530b03a9c6e45ed151b9267
Diego Alonso Ojeda Medina <daom89@gmail.com>
2020-09-29 20:22:25 -0500

    * Setters and Getters por propiedades. * Metodo Destructor. * Metodo toString. * Validaciones por medio de gets y sets

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -3,18 +3,97 @@
 class Carro // UpperCamelCase, { }
 {
     //Propiedades
-    public string $marca; //Visibilidad (public, protected, private)
-    public string $color; // Tipos (bool, int, float, null, array, object)
-    public bool $cajaAutomatica; // LowerCamelCase
-    public float $cantidadGasolina;
+    private string $marca; //Visibilidad (public, protected, private)
+    private string $color; // Tipos (bool, int, float, null, array, object)
+    private bool $cajaAutomatica; // LowerCamelCase
+    private float $cantidadGasolina;
+
+    //Variable
+    private array $marcasExcluidas = array('lexus', 'opel', 'porche');
+
 
     //Metodo Constructor
-    public function __construct ($marca = "Generica", $color = "Rojo", $cajaAutomatica = false)
+    public function __construct ($marca = "Generica", $color = "Rojo", $cajaAutomatica = "No")
     {
-        $this->marca = $marca; //Propiedad recibida y asigna a una propiedad de la clase
+        $this->setMarca($marca); //Propiedad recibida y asigna a una propiedad de la clase
+        $this->setColor($color);
+        $this->setCajaAutomatica($cajaAutomatica);
+        $this->setCantidadGasolina(10); //Por defecto de fabrica salen con 10 litros de gasolina
+    }
+
+    public function __destruct() // Cierro Conexiones
+    {
+        echo "<span style='color: darkred'>";
+            echo $this->getMarca()." se ha destruido<br/>";
+        echo "</span>";
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getMarca() : string
+    {
+        return $this->marca;
+    }
+
+    /**
+     * @param string $marca
+     */
+    public function setMarca (string $marca): void
+    {
+        if ( in_array(strtolower($marca), $this->marcasExcluidas) ){
+            echo "La marca del coche no esta aceptada";
+        }else{
+            $this->marca = $marca;
+        }
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getColor () : string
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed|string $color
+     */
+    public function setColor(string $color): void
+    {
         $this->color = $color;
-        $this->cajaAutomatica = $cajaAutomatica;
-        $this->cantidadGasolina = 10;
+    }
+
+    /**
+     * @return mixed|bool
+     */
+    public function getCajaAutomatica() : string
+    {
+        return ($this->cajaAutomatica) ? "Si" : "No";
+    }
+
+    /**
+     * @param mixed|bool $cajaAutomatica
+     */
+    public function setCajaAutomatica(string $cajaAutomatica): void
+    {
+        $this->cajaAutomatica = strtolower(trim($cajaAutomatica)) == "si";
+    }
+
+    /**
+     * @return float|int
+     */
+    public function getCantidadGasolina() : float
+    {
+        return $this->cantidadGasolina;
+    }
+
+    /**
+     * @param float|int $cantidadGasolina
+     */
+    public function setCantidadGasolina(float $cantidadGasolina): void
+    {
+        $this->cantidadGasolina = $cantidadGasolina;
     }
 
     //Metodo
@@ -33,15 +112,24 @@ class Carro // UpperCamelCase, { }
         return $this;
     }
 
+    public function __toString() : string
+    {
+        return  "<strong>Marca:</strong> ".$this->getMarca()."<br/>".
+                "<strong>Color:</strong> ".$this->getColor()."<br/>".
+                "<strong>Caja Automatica:</strong> ".$this->getCajaAutomatica()."<br/>".
+                "<strong>Cantidad de Gasolina:</strong> ".$this->getCantidadGasolina()."<br/>";
+    }
+
 }
 
-$bmw = new Carro('BMW', 'Azul'); // Crear el objeto bmw de la clase Carro; A esto se le llama instanciacion.
+$bmw = new Carro('BMW Nueo', 'Gris', "No"); // Crear el objeto bmw de la clase Carro; A esto se le llama instanciacion.
 $mercedes = new Carro(); //Segundo Objeto de la clase Objeto
-$audi = new Carro("Audi", "Naranja", true);
+$audi = new Carro("Audi", "Naranja", "Si");
 
 echo $bmw->saludar('Diego');
 echo $mercedes->saludar('Juan');
 echo $audi->saludar('Pedro');
+echo $audi->getMarca()." es de caja automatica: ".$audi->getCajaAutomatica()."<br/>";
 
 $audi->tanquear(20) //30 Litros
     ->viajar(100) // 28 Litros
@@ -50,20 +138,22 @@ $audi->tanquear(20) //30 Litros
     ->viajar(300) // 68 Litros
     ->tanquear(20); //88 Litros
 
-echo "Soy ".$audi->marca." y tengo ".$audi->cantidadGasolina." Litros de Gasolina<br/>";
-echo "Soy ".$bmw->marca." y tengo ".$bmw->cantidadGasolina." Litros de Gasolina<br/>";
+echo $bmw;
+
+echo "Soy ".$audi->getMarca()." y tengo ".$audi->getCantidadGasolina()." Litros de Gasolina<br/>";
+echo "Soy ".$bmw->getMarca()." y tengo ".$bmw->getCantidadGasolina()." Litros de Gasolina<br/>";
 
 //Obtener una propiedad
 //echo $bmw->color."<br/>";   //Para obtener la propiedad de un objeto se usa el conecto ->
 //echo $mercedes->color."<br/>";
 
 //Establecer una propiedad
-$bmw->color = "Azul";   //Para establecer una propiedad se le asigna de la misma manera que una variable
-$bmw->marca = "BMW";
+$bmw->setColor("Azul");   //Para establecer una propiedad se le asigna de la misma manera que una variable
+$bmw->setMarca("BMW");
 //echo "Soy un ".$bmw->marca." ".$bmw->color."<br/>";   //Imprimimos los valores
 
-$mercedes->color = "Negro";
-$mercedes->marca = "Mercedes Benz";
+$mercedes->setColor("Negro");
+$mercedes->setMarca("Mercedes Benz");
 //echo "Soy un ".$mercedes->marca." ".$mercedes->color."<br/>";   //Imprimimos los valores
 
 //Llamar a un metodo
